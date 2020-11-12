30e4a85f8509c1c59b24b5b5ae327028716ed048
quockhanhpro145 <73975123+quockhanhpro145@users.noreply.github.com>
2020-11-12 10:51:48 +0700

    Update variable.c

--- Bài tập 1/variable.c
+++ Bài tập 1/variable.c
@@ -12,7 +12,8 @@ int main(void) {
   // khoi tao gia tri cho bien c
   c = 'a';
   // hien thi bien c ra man hinh
-  printf("Bien c = %c",c);
+  printf("Bien c = %c\n",c);
   //hien thi dong: bien num=5 va bien c=a
+  printf("Bien num = %d\n va bien c = %c\n",num,c);
   return 0;
 }
