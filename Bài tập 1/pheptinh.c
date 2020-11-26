fb5ecc7d81827ac9567e5b8dc7dad029224de83f
quockhanhpro145 <73975123+quockhanhpro145@users.noreply.github.com>
2020-11-26 08:26:43 +0700

    Create pheptinh.c

---
+++ Bài tập 1/pheptinh.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+  // khai bao bien num kieu int
+  int num;
+  // khoi tao gia tri bien num
+  num = 5;
+  //hien thi bien num ra man hinh
+  printf("Bien num= %d\n",num);
+  // khai bao bien x kieu char
+  char c;
+  // khoi tao gia tri cho bien c
+  c = 'a';
+  // hien thi bien c ra man hinh
+  printf("Bien c = %c\n",c);
+  //hien thi dong: bien num=5 va bien c=a
+  printf("Bien num = %d va bien c = %c\n",num,c);
+  return 0;
+}
