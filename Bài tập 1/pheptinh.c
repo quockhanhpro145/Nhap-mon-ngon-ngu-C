a7ee2169d7434948d13deb528a6addd4d33dfb6e
quockhanhpro145 <73975123+quockhanhpro145@users.noreply.github.com>
2020-11-26 10:49:52 +0700

    Update pheptinh.c

--- Bài tập 1/pheptinh.c
+++ Bài tập 1/pheptinh.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
-
-int main(void) {
-  // khai bao bien a, b kieu int
-  int a; int b;
-  // khoi tao gia tri bien a,b
-  a= 3; b= 5;
-  // khai bao bien tong, hieu, thuong, tich kieu int
-  int tong; int hieu; int tich; int thuong;
-  //khoi tao gia tri bien tong, hieu, thuong, tich
-  tong= a+b; hieu= a-b; thuong= a/b; tich= a*b;
-  //hien thi bien tong ra man hinh
-  printf("tong= %d",a+b);
-  //hien thi bien hieu ra man hinh
-  printf("hieu= %d",a-b);
-  //hien thi bien tich ra man hinh
-  printf("tich= %d",a*b);
-  //hien thi bien thuong ra man hinh
-  printf("thuong= %d\n",a/b);
-  double c = 3;
-  double d = 5;
-  printf("thuong = %f", c/d);
-  return 0;
+int main(void)
+{
+  // khai bao bien a,b dang int
+int a,b;
+	a = 3;
+	b = 5;
+	//khai bao bien tong, hieu, tich dang int
+  int tong, hieu, tich;
+	// khai bao bien thuong dang double 
+  double thuong;
+	// khoi tao gia tri bien tong, hieu, tich, thuong
+  tong = a + b;
+	hieu = a - b;
+	tich = a * b;
+	thuong = (double)a / b;
+	// hien thi bien tong, hieu, tich, thuong ra man hinh
+  printf("a + b = %d, a - b = %d, a * b = %d, a / b = %0.1f", tong, hieu, tich, thuong);
+	return 0;
 }
