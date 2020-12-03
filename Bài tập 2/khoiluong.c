0c61c072d3a41c99cee2c7a33d29ab8b26481b41
quockhanhpro145 <73975123+quockhanhpro145@users.noreply.github.com>
2020-12-03 10:45:00 +0700

    Update khoiluong.c

--- Bài tập 2/khoiluong.c
+++ Bài tập 2/khoiluong.c
@@ -4,7 +4,7 @@ int main(void) {
   int m;
   scanf("%d", &m);
   printf("kg = %dg\n--------\n", m * 1000);
-  printf("KG   | G\n");
+  printf("KG   | G\n--------\n");
   printf("%d", m);
   printf("   |%d", m * 1000);
   return 0;
