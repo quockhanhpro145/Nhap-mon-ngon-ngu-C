f2be6b2b6410cedf854f6e69c6cf39c2e6a68bf1
quockhanhpro145 <73975123+quockhanhpro145@users.noreply.github.com>
2020-12-03 08:24:02 +0700

    Create hinhtron.c

---
+++ Bài tập 1/hinhtron.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+  //khai bao bien DT,CV,R 
+  double DT,CV,R;
+  // gan gia tri bien R
+  R = 1.5;
+  // khai bao hang PI
+  const double PI = 3.14;
+  // gan gia tri DT,CV
+  DT = R*R*PI;
+  CV = 2*R*PI;
+  printf("DT = %0.1f\n",DT);
+  printf("CV = %0.1f",CV);
+  return 0;
+}
