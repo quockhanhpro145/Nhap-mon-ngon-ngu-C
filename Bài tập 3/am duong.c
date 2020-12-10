65168cf7fc04d6a7fd040a2fce1e890f9d2b625a
quockhanhpro145 <73975123+quockhanhpro145@users.noreply.github.com>
2020-12-10 08:48:23 +0700

    Update am duong.c

--- Bài tập 3/am duong.c
+++ Bài tập 3/am duong.c
@@ -8,9 +8,11 @@ int main(void) {
   printf("Nhap a = ");
   scanf("%d",&a);
 //kiem ra a la + hay -
-if(a >= 0){
-  printf("%d la so khong am",a);
-} else {
+if(a > 0){
+  printf("%d la so duong",a);
+} else if(a==0) {
+  printf("%d bang khong",a);
+} else{
   printf("%d la so am",a);
 }
 
