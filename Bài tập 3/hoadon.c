367330ed7340374d0d2594f3be86293c8679af8c
quockhanhpro145 <73975123+quockhanhpro145@users.noreply.github.com>
2020-12-17 07:41:24 +0700

    Create hoadon.c

---
+++ Bài tập 3/hoadon.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+  //khai bao bien
+  int a,b;
+  // xuat nhap bien
+  printf("Nhap dongia = "); scanf("%d",&a);
+  printf("Nhap soluong = "); scanf("%d",&b);
+  // tinh thanh tien
+  if(b <= 5){
+  printf("a*b = %d",a*b);
+  }else if(b>5){
+    printf("a*b = %f",a*b*0.75);
+  }
+
+  return 0;
+}
