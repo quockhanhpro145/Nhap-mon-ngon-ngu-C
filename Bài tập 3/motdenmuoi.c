98d4774dad4fb3e474266025e81083fe9843a0a7
quockhanhpro145 <73975123+quockhanhpro145@users.noreply.github.com>
2020-12-24 08:38:19 +0700

    Create motdenmuoi.c

---
+++ Bài tập 3/motdenmuoi.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+  //khai bao bien
+  int num;
+  //xuat ra man hinh 1 den 10 tren 1 hang
+  for(num = 1;num <=10;num +=1)
+      printf("%d \t ",num);
+      printf("\n");
+  // xuat ra man hinh 1 den 10 10 dong
+  for(num = 1;num <=10;num +=1)
+      printf("%d \n ",num);
+  return 0;
+}
