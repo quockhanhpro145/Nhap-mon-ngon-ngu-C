09c17bf3112bbea7c1107a86233e16e2711307f0
quockhanhpro145 <73975123+quockhanhpro145@users.noreply.github.com>
2020-12-24 08:27:59 +0700

    Update xinchao.c

--- Bài tập 3/xinchao.c
+++ Bài tập 3/xinchao.c
@@ -4,7 +4,6 @@ int main(void) {
   //khai bao bien
   int count;
   //xuat ra man hinh 10 dong xin chao
-  printf("hien ra 10 dong ");
   for(count = 1;count <=10;count++)
       printf("\n \t \t xin chao");
   return 0;
