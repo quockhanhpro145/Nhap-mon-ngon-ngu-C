7837dfc995034bf6a64c4f7f4ef365b2090570e0
quockhanhpro145 <73975123+quockhanhpro145@users.noreply.github.com>
2020-12-24 10:54:28 +0700

    Update tong10.c

--- Bài tập 3/tong10.c
+++ Bài tập 3/tong10.c
@@ -2,10 +2,11 @@
 
 int main(void) {
   // khai bao bien
-  int tong,dem;
+  int tong,dem,n;
   tong = 0;
   // xuat ra man hinh tong tu 1 den 10
-  for(dem = 1; dem <=10; dem = dem + 1)
+    printf("Nhap n =");scanf("%d",&n);
+  for(dem = 1; dem <=n; dem = dem + 1)
       tong = tong + dem;
       printf(" Tong la: %d",tong);
 
