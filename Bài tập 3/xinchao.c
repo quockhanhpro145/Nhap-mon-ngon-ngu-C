94c6548f4197d0e291c5b1d33699eecf7eb5527a
quockhanhpro145 <73975123+quockhanhpro145@users.noreply.github.com>
2020-12-24 08:25:27 +0700

    Create xinchao.c

---
+++ Bài tập 3/xinchao.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+  //khai bao bien
+  int count;
+  //xuat ra man hinh 10 dong xin chao
+  printf("hien ra 10 dong ");
+  for(count = 1;count <=10;count++)
+      printf("\n \t \t xin chao");
+  return 0;
+}
