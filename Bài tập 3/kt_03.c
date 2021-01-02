59d7a1be48c8ca82c6a799e7249c950b71beadb8
quockhanhpro145 <73975123+quockhanhpro145@users.noreply.github.com>
2021-01-02 21:20:17 +0700

    Create kt_03.c

---
+++ Bài tập 3/kt_03.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main(){
+  int a;
+  for(a = 1; a < 21; a= a + 2){
+    if(a != 5){
+       if(a != 11){
+        if(a != 15){
+      printf("%d \n",a);
+    }
+    }
+    }
+  }
+return 0;
+}
