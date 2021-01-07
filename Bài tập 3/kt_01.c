51991d9c1e42e8506b9ad59f084770580458e379
quockhanhpro145 <73975123+quockhanhpro145@users.noreply.github.com>
2021-01-07 10:36:09 +0700

    Update kt_01.c

--- Bài tập 3/kt_01.c
+++ Bài tập 3/kt_01.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
- 
+#include <math.h>
+
 int main(){
-  int i,j;
-    for(i = 1; i <= 9;i++){
-        printf("\nBANG NHAN %d\n", i);
-        for(j = 0; j <= 10; j++){
-            printf("%d x %d = %d\n", i, j, i*j);
-        }
-    }
+  int tich1,tich2,tich3,tich4;
+  tich1= 1,tich2 = 2, tich3= 3, tich4= 4;
+  printf("bang cuu chuong\n");
+  printf("1 x 1 = %d\t",tich1 * 1);
+  printf("1 x 2 = %d\t",tich1 * 2);
+  printf("1 x 3 = %d\t",tich1 * 3);
+  printf("1 x 4 = %d\t",tich1 * 4);
+  printf("\n");
+  printf("2 x 1 = %d\t",tich2 * 1);
+  printf("2 x 2 = %d\t",tich2 * 2);
+  printf("2 x 3 = %d\t",tich2 * 3);
+  printf("2 x 4 = %d\t",tich2 * 4);
+  printf("\n");
+  printf("3 x 1 = %d\t",tich3 * 1);
+  printf("3 x 2 = %d\t",tich3 * 2);
+  printf("3 x 3 = %d\t",tich3 * 3);
+  printf("3 x 4 = %d\t",tich3 * 4);
+  printf("\n");
+  printf("4 x 1 = %d\t",tich4 * 1);
+  printf("4 x 2 = %d\t",tich4 * 2);
+  printf("4 x 3 = %d\t",tich4 * 3);
+  printf("4 x 4 = %d\t",tich4 * 4);
+  
+  
+return 0;
 }
