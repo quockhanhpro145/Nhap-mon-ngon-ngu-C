6d205522fe42bc98e5b3b9468a745d890bb43f6c
quockhanhpro145 <73975123+quockhanhpro145@users.noreply.github.com>
2021-01-07 07:33:04 +0700

    Update kt_02.c

--- Bài tập 3/kt_02.c
+++ Bài tập 3/kt_02.c
@@ -1,27 +1,13 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(void) {
-  // khai báo biến
-  float a, b, c, delta, x1, x2, x;
-  // nhập xuất giá trị
+int main(){
+	float a, b, c, delta, x1, x2;
   printf("Nhap a = ");scanf("%f",&a);
   printf("Nhap b = ");scanf("%f",&b);
   printf("Nhap c = ");scanf("%f",&c);
-  if(a == 0){// pt bậc 1
-  if(b == 0 && c != 0){
-    printf("Phuong trinh vo nghiem\n");
-  }
-  else if(b == 0 && c == 0){
-    printf("Phuong trinh co vo so nghiem\n",x);
-  }
-  else{
-    x= -c / b;
-    printf("phuong trinh co nghiem = %f",x);
-  }
-  }else{// pt bậc 2
-      delta = b * b - 4 * a * c;
-      if(delta < 0){
+  delta = b * b - 4 * a * c;
+  if(delta < 0){
         printf("Phuong trinh vo nghiem");
       }else if(delta == 0){
         x1 = -b / (a * 2);
@@ -31,6 +17,5 @@ int main(void) {
         x2 = (-b - sqrt(delta)) / (a * 2);
         printf("Phuong co nghiem\n x1 = %f\n x2= %f",x1, x2);
       }
-  }
-  return 0;
+return 0;
 }
