1991742e09d51dfb6376af0ba11c70c2b334762a
quockhanhpro145 <73975123+quockhanhpro145@users.noreply.github.com>
2021-01-07 08:53:45 +0700

    Update review_01.c

--- Bài tập 3/review_01.c
+++ Bài tập 3/review_01.c
@@ -7,25 +7,25 @@ int main(void) {
   printf("Nhap n = ");scanf("%d",&n);
 //in từ tiếng anh ứng với số 1 đến 9
   if(n == 1){
-    printf("One",n);
+    printf("one");
   }else if(n == 2){
-    printf("Two",n);
+    printf("two");
   }else if(n == 3){
-    printf("Three",n);
+    printf("three");
   }else if(n == 4){
-    printf("Four",n);
+    printf("four");
   }else if(n == 5){
-    printf("Five",n);
+    printf("five");
   }else if(n == 6){
-    printf("Six",n);
+    printf("six");
   }else if(n == 7){
-    printf("Seven",n);
+    printf("seven");
   }else if(n == 8){
-    printf("Eight",n);
+    printf("eight");
   }else if(n == 9){
-    printf("nice",n);
-  }else if(n >9){
-    printf(" Greater than 9 "); 
+    printf("nine");
+  }else {
+    printf("Greater than 9"); 
   }
   return 0;
 }
