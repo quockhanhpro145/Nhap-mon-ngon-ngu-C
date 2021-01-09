84dfb5c8d691ee5c4bcabb59faa31deb3dadcc87
quockhanhpro145 <73975123+quockhanhpro145@users.noreply.github.com>
2021-01-09 10:41:32 +0700

    Update kt_06.c

--- Bài tập 3/kt_06.c
+++ Bài tập 3/kt_06.c
@@ -5,13 +5,13 @@
   //hang 3:5 sao 1 khoang trang
   //hang 4:7 sao 0 khoang trang
   //h = 3: 3 hang
-  //hang 1:1 sao 3 khoang trang
-  //hang 2:3 sao 2 khoang trang
-  //hang 3:5 sao 1 khoang trang
+  //hang 1:1 sao 2 khoang trang
+  //hang 2:3 sao 1 khoang trang
+  //hang 3:5 sao 0 khoang trang
   //h = 2: 2 hang
-  //hang 1:1 sao 3 khoang trang
-  //hang 2:3 sao 2 khoang trang
+  //hang 1:1 sao 1 khoang trang
+  //hang 2:3 sao 0 khoang trang
   //h = 1: 1 hang
-  //hang 1:1 sao 3 khoang trang
+  //hang 1:1 sao 0 khoang trang
 
   
