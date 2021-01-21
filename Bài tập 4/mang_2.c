1769dd319298ca65640cbbb6772136689026d2c3
quockhanhpro145 <73975123+quockhanhpro145@users.noreply.github.com>
2021-01-21 08:33:49 +0700

    Update mang_2.c

--- Bài tập 4/mang_2.c
+++ Bài tập 4/mang_2.c
@@ -4,10 +4,13 @@ int main(){
     int a[100],n,tong;
     tong = 0;
     printf("Nhap n:");scanf("%d",&n);
-    for(n = 0; n < 100; n++)
+    for(int i = 0; i < n; i++){
+     printf("\nNhap phan tu a[%d] = ", i);
+     scanf("%d", &a[i]);
+     }
     printf("%d\n",a[n]);
-    for(n = 0; n < 100; n++)
-    tong = tong + a[n];
+    for(int i = 0; i < n; i++)
+    tong = tong + a[i];
     printf("Tong = %d",tong);
     getchar();
     return 0;
