26acc349ae2e7efb957019dee5dcd638a9fae82d
quockhanhpro145 <73975123+quockhanhpro145@users.noreply.github.com>
2021-01-28 14:55:46 +0700

    Update TH_02.c

--- Bài tập 5/TH_02.c
+++ Bài tập 5/TH_02.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+int main(){
+  int n,s;
+  s = 0;
+  printf("Nhap n = ");
+  scanf("%d",&n);
+  for(int i = 1; i <= n; i++)
+  s = s + i;
+  printf("\nS = 1 + 2 +...+  = %d ",s);
+  return 0; 
+}
