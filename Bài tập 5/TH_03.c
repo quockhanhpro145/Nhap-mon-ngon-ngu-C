c3f1562b6605a91f9e532d5cac91e38fb716e3db
quockhanhpro145 <73975123+quockhanhpro145@users.noreply.github.com>
2021-01-28 15:19:13 +0700

    Create TH_03.c

---
+++ Bài tập 5/TH_03.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+  int a,b,c,n;
+
+  printf("Nhap a = ");scanf("%d",&a);
+  printf("Nhap b = ");scanf("%d",&b);
+  printf("Nhap c = ");scanf("%d",&c);
+  if(a > b && a > c)
+    {
+    printf("max la : %d",a);
+    }
+   else if(b > a && b > c)
+    {
+      printf("max la : %d",b);
+    }
+  else if(c > a && c > b )
+    {
+      printf("max la : %d",c);
+    }
+  return 0;
+}
