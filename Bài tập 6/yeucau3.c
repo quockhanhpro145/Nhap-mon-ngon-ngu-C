07bb7b5dece8a175b75be10e60b1b14b61a08f62
quockhanhpro145 <73975123+quockhanhpro145@users.noreply.github.com>
2021-03-20 08:55:15 +0700

    Create yeucau3.c

---
+++ Bài tập 6/yeucau3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int UCLN(int a, int b){
+     if (a == 0 || b == 0){
+        return a + b;
+    }
+    while (a != b){
+        if (a > b){
+            a -= b; // a = a - b
+        }else{
+            b -= a;
+        }
+    }
+    return a;
+   }
+int main(void) {
+   int a,b;
+   printf("Nhap a = ");scanf("%d",&a);
+   printf("Nhap b = ");scanf("%d",&b);
+   printf("UCLN = %d",UCLN(a,b));
+   return 0;
+}
